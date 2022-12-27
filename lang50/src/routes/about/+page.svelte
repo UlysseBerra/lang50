c621736ee693e95b1e30c237baa9270c58dfe12b
Aurélien Berra <aurelien.berra@gmail.com>
2022-12-27 11:05:28 +0100

    initial front-end with svelte-kit

---
+++ lang50/src/routes/about/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+    <title>About</title>
+    <meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+    <h1>About <code>lang50</code></h1>
+
+    <p>You know what <code>lang50</code> is, right?</p>
+</div>
---
+++ lang50/src/routes/leaderboard/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+    <title>Leaderboard</title>
+    <meta name="description" content="Leaderboard" />
+</svelte:head>
+
+<div class="text-column">
+    <h1>Leaderboard</h1>
+
+    <p>Here soon: a list of users and scores</p>
+</div>
