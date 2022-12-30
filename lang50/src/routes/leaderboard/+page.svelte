c124273687d2b0dfa352d870f13635f2a85aec5e
Aurélien Berra <aurelien.berra@gmail.com>
2022-12-31 00:03:51 +0100

    test fetch local json file

--- lang50/src/routes/leaderboard/+page.svelte
+++ lang50/src/routes/leaderboard/+page.svelte
@@ -1,9 +1,15 @@
 <script>
-    const url = "https://jsonplaceholder.typicode.com/users";
-    const fetchUsers = (async () => {
-        const response = await fetch(url);
-        let users = await response.json();
-        return users;
+    // const url = "https://jsonplaceholder.typicode.com/users";
+    // const fetchUsers = (async () => {
+    //     const response = await fetch(url);
+    //     let users = await response.json();
+    //     return users;
+    // })();
+
+    const fetchTestData = (async () => {
+        const response = await fetch("test.json");
+        let test_data = await response.json();
+        return test_data;
     })();
 </script>
 
@@ -16,7 +22,36 @@
     <h1>Leaderboard</h1>
 </div>
 
-{#await fetchUsers}
+{#await fetchTestData}
+    <p>...waiting</p>
+{:then test_data}
+    <div class="overflow-x-auto">
+        <table class="table table-compact w-full">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Username</th>
+                    <th>Score</th>
+                </tr>
+            </thead>
+            <tbody>
+                {#each test_data as user}
+                    <tr>
+                        <td>{user.id}</td>
+                        <td>{user.name}</td>
+                        <td>{user.username}</td>
+                        <td>{user.score}</td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+    </div>
+{:catch error}
+    <p>An error occurred!</p>
+{/await}
+
+<!-- {#await fetchUsers}
     <p>...waiting</p>
 {:then users}
     <div class="overflow-x-auto">
@@ -41,4 +76,4 @@
     </div>
 {:catch error}
     <p>An error occurred!</p>
-{/await}
+{/await} -->
