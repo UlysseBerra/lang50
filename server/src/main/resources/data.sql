92ed6eba39734cc8b2b0fe45387d72238848dc03
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2023-01-01 23:02:17 +0100

    made db file instead of memory

---
+++ server/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO languages (name, audio, script) VALUES
+    ('Test Language', FILE_READ('/Users/ulysseberra/Documents/Code/lang50/server/src/main/resources/static/tmp.mp3'), FILE_READ('/Users/ulysseberra/Documents/Code/lang50/server/src/main/resources/static/tmp.jpg'));
