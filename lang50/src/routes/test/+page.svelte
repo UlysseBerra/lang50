a76a0c36659d088972ff97ecc21002464f968050
Aurélien Berra <aurelien.berra@gmail.com>
2023-01-02 01:45:47 +0100

    update audio import from base64 in json file

--- lang50/src/routes/test/+page.svelte
+++ lang50/src/routes/test/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
     import AudioControls from "$lib/components/AudioControls.svelte";
-    // import AudioPlayer, { stopAll } from "$lib/components/AudioPlayer.svelte";
-    import { AudioPlayer } from "svelte-mp3";
+    import AudioPlayer, { stopAll } from "$lib/components/AudioPlayer.svelte";
     import idontunderstand_el from "$lib/audio/idontunderstand_el.mp3";
     let audioTrack = idontunderstand_el;
+    // let audioTrack = "https://sveltejs.github.io/assets/music/satie.mp3";
 
     // Encode/decode string with atob/btoa
     // let encodedStr = "SGVsbG9AV29ybGQ=";
@@ -17,14 +17,12 @@
 
     // Import ASCII from JSON
     // @TODO how to play it after proper decoding?
-    import encoded from "$lib/audio/test_audio2.json";
-    // let decoded = window.atob(audio);
-    let audio = encoded.audio.content;
-    // test = window.atob(audio);
-    // let test = new Audio("data:audio/ogg;base64,".concat(audio));
-    // console.log(audio);
-    let decoded = window.btoa(audio);
-    // console.log("DECODED: ", decoded);
+    // import encoded from "$lib/audio/test_audio2.json";
+    import encoded from "$lib/audio/tmp.json";
+    const audio = encoded.audio;
+    // const test = new Audio(`data:audio/mp3;base64,${audio}`);
+    const test = `data:audio/mp3;base64,${audio}`;
+    console.log(test)
 </script>
 
 <svelte:head>
@@ -37,13 +35,10 @@
 
     <!-- <AudioPlayer src={audioTrack} /> -->
 
-    <!-- @TODO play audio from base64 decoded data -->
 </div>
 
-<!-- @NOTE can't get the default audio tag to work -->
-
 <!-- AudioControls component from https://github.com/Linkcube/svelte-audio-controls -->
-<div>
+<!-- <div>
     <AudioControls
         src="https://sveltejs.github.io/assets/music/satie.mp3"
         display={true}
@@ -51,4 +46,36 @@
         barPrimaryColor="#661AE6"
         barSecondaryColor="#661AE6"
     />
+</div> -->
+
+
+<div><audio controls> <!-- autoplay -->
+    <source src="{test}" type="audio/mp3">
+    </audio></div>
+<div class="divider divider-vertical m-10">OR</div>
+<div><AudioControls
+    src={test}
+    display={true}
+    iconColor="#D926AA"
+    barPrimaryColor="#661AE6"
+    barSecondaryColor="#661AE6"
+/></div>
+
+<div class="m-10"></div>
+
+<div class="flex w-full">
+  <div class="grid h-20 flex-grow place-items-center"><audio controls preload="none">
+    <!--
+        autoplay 	Makes the audio start playing automatically
+        loop 	    Makes the audio start playing again when it finishes
+        muted 	    Makes the player muted by default
+        preload 	Instructs the browser what to preload – the whole file (auto), its metadata (metadata) or nothing at all (none)
+    -->
+    <source src="{test}" type="audio/mp3">
+    </audio></div>
+  <div class="divider divider-horizontal">OR</div>
+  <div class="grid h-20 flex-grow place-items-center"><AudioControls
+    src={test}
+    display={true}
+/></div>
 </div>
