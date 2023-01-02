ef533e150b55edc5918dd55b2108f235c7ae1e83
Aurélien Berra <aurelien.berra@gmail.com>
2023-01-02 12:22:26 +0100

    update home

--- lang50/src/routes/+page.svelte
+++ lang50/src/routes/+page.svelte
@@ -17,12 +17,29 @@
         </span>
     </h1>
 
-    <p class="text-center">
-        The <code>lang50</code> project is a joint venture of Ulysse Berra and Aurélien
-        Berra.
-    </p>
-    <p class="text-center">
-        It serves as our Harvard CS50x final project, and modestly aims at
-        changing the world – one nerdy game at a time.
-    </p>
+    <div>
+        <p class="mt-20 mb-10">
+            The <strong><code>lang50</code></strong> project is a joint venture
+            of
+            <strong>Ulysse Berra</strong>
+            and <strong>Aurélien Berra</strong>. It serves as our
+            <a
+                href="https://www.edx.org/course/introduction-computer-science-harvardx-cs50x"
+                >Harvard CS50x</a
+            >
+            final project, and modestly aims at changing the world – one nerdy game
+            at a time. We use the following technologies: design and wireframing
+            with
+            <a href="https://www.figma.com/">Figma </a>; back-end in
+            <a href="https://www.java.com/">Java</a> with
+            <a href="https://spring.io/">Spring Boot</a>
+            and <a href="http://h2database.com/">H2 database</a>; front-end with
+            <a href="https://svelte.dev/">Svelte</a> and
+            <a href="https://kit.svelte.dev/">SvelteKit</a>,
+            <a href="https://www.typescriptlang.org/">TypeScript</a>,
+            <a href="https://tailwindcss.com/">Tailwind</a> and
+            <a href="https://daisyui.com/">daisyUI</a>
+            .
+        </p>
+    </div>
 </section>
