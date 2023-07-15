0dcc640c7b39bdd460fd53327dd6a70e58350264
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2023-07-15 18:35:31 +0200

    basic auth system

--- server/src/main/resources/import.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO languages (id, name, audio, script) VALUES (1, 'Test Language', FILE_READ('/Users/ulysseberra/Documents/Code/lang50/server/src/main/resources/static/tmp.mp3'), FILE_READ('/Users/ulysseberra/Documents/Code/lang50/server/src/main/resources/static/tmp.jpg'));
