8d76941a6a96e502604c76f1f1de9b438d150dab
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2023-08-11 10:15:21 +0200

    cors

--- server/app.py
+++ server/app.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from src.db import initialize_database
 from src.routes import router
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
